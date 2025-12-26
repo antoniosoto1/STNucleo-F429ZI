c42d57dfab760589590eeadf1c514513c45b5e6c
ANROSO <antoniosoto@iteso.mx>
2025-12-26 09:04:28 -0600

    UART Simple, no extra HW needed

--- main.cpp
+++ main.cpp
@@ -5,27 +5,15 @@
 
 #include "mbed.h"
 
-#define WAIT_TIME_MS 500 
-DigitalOut led1(LED1);
-DigitalOut led3(LED3);
-DigitalOut led2(LED2);
-DigitalIn userbutt(BUTTON1);
+BufferSerial pc(USBTX,USBRX,9600)
+int cycle=0;
 
 int main()
 {
-    printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
-
     while (true)
     {
-        led1 = !led1;
+        printf("This is the bare metal Serail Comm Loop\n\r",cycle);
         thread_sleep_for(WAIT_TIME_MS);
-        if (userbutt)
-        {
-            led3 = 1;
-            led2 =!led2;
-        }else {
-            led3 = 0;
-            led2 =1;
-        }
+        cycle++;
     }
 }
